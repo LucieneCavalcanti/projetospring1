b0901c77c0445f6022a0b3b88128e7c86686460f
LucieneCavalcanti <lucienecavalcanti@live.com>
2024-05-22 09:06:23 -0300

    Projeto inicial

---
+++ src/main/java/br/edu/fatec/projetospring1/Projetospring1Application.java
@@ -0,0 +1,13 @@
+package br.edu.fatec.projetospring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetospring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetospring1Application.class, args);
+	}
+
+}
---
+++ src/main/java/br/edu/fatec/projetospring1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.edu.fatec.projetospring1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Projetospring1Application.class);
+	}
+
+}
